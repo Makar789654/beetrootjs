d40da358a44a28d61c8d4ab28ccf45ed6225b961
Makar789654 <112611445+Makar789654@users.noreply.github.com>
2022-12-09 15:10:34 +0100

    2/3

---
+++ 09.12js1les/src/test.js
@@ -0,0 +1,26 @@
+let usersNames = "Roman Makarets";
+let userNamesData = "Roman Makarets";
+let userFullName = "Roman Makarets";
+let userNameSurname = "Roman Makarets";
+
+// !name 
+// 1name
+// .datauser 
+
+// 123
+// one sting //
+// lot of strings /* tlalala */ 
+// 123
+
+// camelCase 
+// kebab_case 
+
+const thisYear = 2022;
+let userName = prompt("what is your name?"),
+    age = +prompt("what is your year of birth?")
+    alert("Hi, " +  userName);
+    alert("Your age is " + (thisYear - age));
+
+let squareLength = prompt(
+    "What is the length of the side of the square?")
+    alert("The perimeter of a square is " + (4 * squareLength));
